d441fc314e8ac30646006fda8b9e9938bb6fc28e
digambar2002 <digambarchaudhari425303@gmail.com>
2023-10-22 18:28:19 +0530

    day 6 completed

--- engine/command.py
+++ engine/command.py
@@ -1,5 +1,5 @@
 import pyttsx3
-
+import speech_recognition as sr
 
 def speak(text):
     engine = pyttsx3.init('sapi5')
@@ -10,4 +10,27 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-speak("India is my country, all indians are my brothers and sisters")
+def takecommand():
+
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print('listening....')
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+        
+        audio = r.listen(source, 10, 6)
+
+    try:
+        print('recognizing')
+        query = r.recognize_google(audio, language='en-in')
+        print(f"user said: {query}")
+    except Exception as e:
+        return ""
+    
+    return query.lower()
+
+
+text = takecommand()
+
+speak(text)
