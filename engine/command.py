c74dedb262b6f58a2fd90d37cfd55b075de1a9b6
digambar2002 <digambarchaudhari425303@gmail.com>
2024-03-03 17:21:52 +0530

    day 14 completed

--- engine/command.py
+++ engine/command.py
@@ -9,6 +9,7 @@ def speak(text):
     engine.setProperty('rate', 174)
     eel.DisplayMessage(text)
     engine.say(text)
+    eel.receiverText(text)
     engine.runAndWait()
 
 
@@ -43,9 +44,10 @@ def allCommands(message=1):
     if message == 1:
         query = takecommand()
         print(query)
+        eel.senderText(query)
     else:
         query = message
-
+        eel.senderText(query)
     try:
 
         if "open" in query:
--- www/controller.js
+++ www/controller.js
@@ -18,4 +18,36 @@ $(document).ready(function () {
         $("#Siriwave").attr("hidden", true);
     }
 
+    eel.expose(senderText)
+    function senderText(message) {
+        var chatBox = document.getElementById("chat-canvas-body");
+        if (message.trim() !== "") {
+            chatBox.innerHTML += `<div class="row justify-content-end mb-4">
+            <div class = "width-size">
+            <div class="sender_message">${message}</div>
+        </div>`; 
+    
+            // Scroll to the bottom of the chat box
+            chatBox.scrollTop = chatBox.scrollHeight;
+        }
+    }
+
+    eel.expose(receiverText)
+    function receiverText(message) {
+
+        var chatBox = document.getElementById("chat-canvas-body");
+        if (message.trim() !== "") {
+            chatBox.innerHTML += `<div class="row justify-content-start mb-4">
+            <div class = "width-size">
+            <div class="receiver_message">${message}</div>
+            </div>
+        </div>`; 
+    
+            // Scroll to the bottom of the chat box
+            chatBox.scrollTop = chatBox.scrollHeight;
+        }
+        
+    }
+
+
 });
