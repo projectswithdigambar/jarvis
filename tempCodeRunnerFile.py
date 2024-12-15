882807dad1b61314cfe12d61153e84aff84713f8
ritikthakur0623 <158048018+ritikthakur0623@users.noreply.github.com>
2024-12-15 17:07:53 +0530

    `Update .venv subproject to dirty commit 3f12777b539aa80bc71b55dd189f40948affbaac`

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,29 @@
+import os
+import eel
+
+from engine.features import *
+from engine.command import *
+from engine.auth import recoganize
+def start():
+    
+    eel.init("www")
+
+    playAssistantSound()
+    @eel.expose
+    def init():
+        subprocess.call([r'device.bat'])
+        eel.hideLoader()
+        speak("Ready for Face Authentication")
+        flag = recoganize.AuthenticateFace()
+        if flag == 1:
+            eel.hideFaceAuth()
+            speak("Face Authentication Successful")
+            eel.hideFaceAuthSuccess()
+            speak("Hello, Welcome Sir, How can i Help You")
+            eel.hideStart()
+            playAssistantSound()
+        else:
+            speak("Face Authentication Fail")
+    os.system('start msedge.exe --app="http://localhost:8000/index.html"')
+
+    eel.start('index.html', mode=None, host='localhost', block=True)
