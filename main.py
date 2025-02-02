125b1c81d27e095f3e0f26c45efc123df7e678a1
Meet Jamsutkar <91883747+MeJaM35@users.noreply.github.com>
2025-02-02 08:35:51 +0530

    Remove Face Authentication

--- main.py
+++ main.py
@@ -13,17 +13,10 @@ def start():
     def init():
         subprocess.call([r'device.bat'])
         eel.hideLoader()
-        speak("Ready for Face Authentication")
-        flag = recoganize.AuthenticateFace()
-        if flag == 1:
-            eel.hideFaceAuth()
-            speak("Face Authentication Successful")
-            eel.hideFaceAuthSuccess()
-            speak("Hello, Welcome Sir, How can i Help You")
-            eel.hideStart()
-            playAssistantSound()
-        else:
-            speak("Face Authentication Fail")
+        speak("Hello, Welcome Sir, How can i Help You")
+        eel.hideStart()
+        playAssistantSound()
+
     os.system('start msedge.exe --app="http://localhost:8000/index.html"')
 
     eel.start('index.html', mode=None, host='localhost', block=True)
