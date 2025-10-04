0696fcad043f777888034a4510fe8d46aea137db
digambar2002 <digambar2002c@gmail.com>
2025-10-04 16:48:02 +0530

    Added settings modal

--- engine/db.py
+++ engine/db.py
@@ -4,8 +4,8 @@ import sqlite3
 con = sqlite3.connect("jarvis.db")
 cursor = con.cursor()
 
-query = "CREATE TABLE IF NOT EXISTS sys_command(id integer primary key, name VARCHAR(100), path VARCHAR(1000))"
-cursor.execute(query)
+# query = "CREATE TABLE IF NOT EXISTS sys_command(id integer primary key, name VARCHAR(100), path VARCHAR(1000))"
+# cursor.execute(query)
 
 # query = "INSERT INTO sys_command VALUES (null,'one note', 'C:\\Program Files\\Microsoft Office\\root\\Office16\\ONENOTE.exe')"
 # cursor.execute(query)
